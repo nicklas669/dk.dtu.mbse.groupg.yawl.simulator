dde03796abd87557cdd1f4e75c451de8d061c6c9
Stefan Hyltoft <stefanhyltoft@gmail.com>
2016-04-13 14:50:20 +0200

    Har rettet så der arves fra anden klasse. Påbegyndt initializeContents() implementation.

--- src/dk/dtu/mbse/groupg/yawl/simulator/application/SimulatorApplication.java
+++ src/dk/dtu/mbse/groupg/yawl/simulator/application/SimulatorApplication.java
@@ -1,20 +1,28 @@
 package dk.dtu.mbse.groupg.yawl.simulator.application;
 
-import org.eclipse.gmf.runtime.common.ui.services.parser.GetParserOperation;
 import org.pnml.tools.epnk.annotations.netannotations.NetAnnotation;
 import org.pnml.tools.epnk.annotations.netannotations.NetannotationsFactory;
 import org.pnml.tools.epnk.applications.ApplicationWithUIManager;
 import org.pnml.tools.epnk.applications.ui.ApplicationUIManager;
 import org.pnml.tools.epnk.pnmlcoremodel.PetriNet;
 
-public class SimulatorApplication extends ApplicationUIManager {
+import dk.dtu.mbse.groupg.yawl.simulator.annotations.AnnotationsFactory;
+import dk.dtu.mbse.groupg.yawl.simulator.annotations.AnnotationsPackage;
+import dk.dtu.mbse.groupg.yawl.simulator.annotations.EnabledTransition;
+import dk.dtu.mbse.groupg.yawl.simulator.annotations.Marking;
+import dk.dtu.mbse.groupg.yawl.simulator.annotations.SelectArc;
 
-	// Create a constructor for this class with Petri net as its parameter
-	public SimulatorApplication(ApplicationWithUIManager application) {
-		super(application);
-		// TODO Auto-generated constructor stub
-	}
+public class SimulatorApplication extends ApplicationWithUIManager {
 	
+	
+	/**
+	 * Create a constructor for this class with Petri net as its parameter
+	 * @param petrinet
+	 */
+	public SimulatorApplication(PetriNet petrinet) {
+		super(petrinet);
+	}
+
 	/*
 	 * One method that you must implement is initializeContents(), which creates
 	 * the initial annotations of the net on which the application is started.
@@ -26,25 +34,46 @@ public class SimulatorApplication extends ApplicationUIManager {
 		 * annotation first:
 		 */
 		NetAnnotation netannotation = NetannotationsFactory.eINSTANCE.createNetAnnotation();
-		netannotation.setNet(net);
-		annotat
+		netannotation.setNet(getPetrinet());
+		
+		/**
+		 * For the YAWL simulator, you would need annotations for enabled transitions,
+		 *  for arcs which the user can select from (for XOR-joins and XOR- and OR-splits)
+		 *  and for markings (note that for OR-joins, the user does not need to select arcs,
+		 *  becaise a token will be consumed from all in-coming arcs from a place
+		 *  which has at least one token in it).
+		 */	
+
+		
 		/*
 		 * Then create the corresponding annotations for each object that should
-		 * have an overlay in the initial situation; for each object annotation,
-		 * you must set the underlying Petri net object that it annotates; and
-		 * for the the new Annotaions defined in 2., you also need to set the
-		 * other features and references.
+		 * have an overlay in the initial situation; 
 		 */
+		EnabledTransition enabledTransition = AnnotationsFactory.eINSTANCE.createEnabledTransition();
+		SelectArc selectArc = AnnotationsFactory.eINSTANCE.createSelectArc();
+		//Marking marking = AnnotationsFactory.eINSTANCE.createMarking();
+		
+		//for each object annotation, you must set the underlying Petri net object that it annotates;
+
+		/*		
+		 and for the the new Annotaions defined in 2., you also need to set the
+		 * other features and references.
+		 */		
 
 		// Then, add it to the netannotation by calling
-		// netannotation.getObjectAnnotations().add()
+		 netannotation.getObjectAnnotations().add(enabledTransition);
+		 netannotation.getObjectAnnotations().add(selectArc);
+		 //netannotation.getObjectAnnotations().add(marking);
 
 		/*
 		 * In the end, add this annotation to the application and set it as the
 		 * applications current annotation:
-		 * this.getNetAnnotations().getNetAnnotations().add(initialAnnotation);
-		 * this.getNetAnnotations().setCurrent(initialAnnotation);
 		 */
+		 this.getNetAnnotations().getNetAnnotations().add(netannotation);
+		 this.getNetAnnotations().setCurrent(netannotation);
+		
 	}
+
+
 
 }
